d18baad34a0fbc0cf180a233d0a6a0f3c8d8fe7e
kaaavishh <164745537+kaaavishh@users.noreply.github.com>
2024-10-02 12:59:16 +0000

    conversion

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,8 @@
+let score="33";
+let valueInNumber=Number(score);
+console.log(valueInNumber);
+//if score would have bee "33abc" then also data type will be Number but value would be Nan
+//null 0
+//undefined Nan
+//"kavish" Nan
+//Boolean=> 1 is true 0,"" is false, "kavish" is true 
