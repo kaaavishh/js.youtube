5d75e072be9137db030e9437b94a2e9cbb4f830b
kaaavishh <164745537+kaaavishh@users.noreply.github.com>
2024-10-02 12:55:51 +0000

    conversion of data types

---
+++ 01_basics/01_var.js
@@ -0,0 +1,6 @@
+const accountId=14463
+let accountEmail="dhamkavish@gmail.com"
+var accountPassword="12345"//not used now as it has problem with scope
+accCity="Jaipur" 
+
+console.table([accountId,accountPassword,accountEmail]);
