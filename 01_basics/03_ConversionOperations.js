a7ed4531f911d9048052e1c4b0d353391032e379
kaaavishh <164745537+kaaavishh@users.noreply.github.com>
2024-10-02 14:10:01 +0000

    ...

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -6,3 +6,21 @@ console.log(valueInNumber);
 //undefined Nan
 //"kavish" Nan
 //Boolean=> 1 is true 0,"" is false, "kavish" is true 
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log(1+2+"3");//33//this is messy code
+console.log(+true);//tricky conversion
+
+let num1=num2=num3=2;
+//or
+let n1,n2,n3=2;
+
+console.log({n1,n2,num3});
+//comparison converts null to number where as equal to == doesnt
+console.log(null==0);//false
+console.log(null>=0);//true
+
+// === strict check also checks datatype
+console.log(2=="2");//true
+console.log(2==="2");//false
+//clean code
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+//2 types of dataTypes 
+//Primitive =>call by value
+
+//7 types: string,Number,Boolean,ull,undefined,symbol,BigInt
+
+ //dynamically typed js
+ //c++ is statically typed
+   
+//Reference (Non-Primitive)
+
+//Array,Objects,Functions
+const heroes=["kavish","batman"];//array
+let obj={
+    name:"kavish",
+    age:"19",
+}
+const myFunction=function(){
+    console.log("Hello world");
+}
+//study return type of typeof function asked in intrrviews
