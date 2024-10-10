f8b8b6da8a8c9ce11e445cf3cde845374e8cfbf7
kaaavishh <164745537+kaaavishh@users.noreply.github.com>
2024-10-10 18:57:38 +0000

    commit

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,12 +1,12 @@
 //2 types of dataTypes 
-//Primitive =>call by value
+//Primitive =>call by value//copy milti hai//stack memory
 
 //7 types: string,Number,Boolean,ull,undefined,symbol,BigInt
 
  //dynamically typed js
  //c++ is statically typed
    
-//Reference (Non-Primitive)
+//Reference (Non-Primitive)//heap memory
 
 //Array,Objects,Functions
 const heroes=["kavish","batman"];//array
---
+++ 01_basics/strings.js
@@ -0,0 +1,11 @@
+//conactenation
+
+//interpolation
+const name="kkkkaavish";
+const rCount=6;
+console.log(`Hello mu name is ${name} and my repo count is ${rCount}`);
+const str=new String("kavish");
+str;
+console.log(typeof (str));
+console.log(name.indexOf('a'));
+console.log(name.includes("kkkk"));
